55d7beb1d0010b3b9a465e599ed2a04871ad4fde
Austin Baik <baik.austin@gmail.com>
2021-12-22 13:31:08 -0800

    commit tree 89df8684a3964b0be842f73a54bd06fc89de3bae

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+function saturdayFun(name = "roller-skate") {
+    return `This Saturday, I want to ${name}!`;
+}
+
+
+const mondayWork = function(task = "go to the office"){
+    return `This Monday, I will ${task}.`
+}
+
+function wrapAdjective(flair = "*") {
+    return function(name = "special"){
+        
+        return `You are ${flair}${name}${flair}!`
+    }
+}
